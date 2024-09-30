641ee8820b50f431c90bd13bf6c44e62e9384ef9
A716\1dam <diegogardiol6@gmail.com>
2024-09-30 20:09:16 +0200

    no message

--- EJERCICIOS-Diego_Gardiol/src/main/java/org/example/Practica3.java
+++ EJERCICIOS-Diego_Gardiol/src/main/java/org/example/Practica3.java
@@ -2,24 +2,22 @@ package org.example;
 import java.util.Scanner;
 public class Practica3 {
     public void practica3(){
-        System.out.println("*******************************");
-        System.out.println("******INTRODUCE TU NOMBRE******");
-        System.out.println("*******************************");
+        System.out.println("******INTRODUCE TU NOMBRE******");//Aparece por pantalla el texto de introducir nombre
+
         Scanner entrada = new Scanner(System.in);
+        String nombre = entrada.next();//Lee la entrada de el teclado y lo guarda en una variable nombre en formato string
 
-        String nombre = entrada.next();
+        System.out.println("Hola "+ nombre + ". INTRODUCE TU EDAD:");//Aparecera por pantalla el texto + la variable nombre para mostrar el valor de la variable
+        int edad = entrada.nextInt();//Ahora leemos la entrada de el teclado y guardarlo en la variable edad en formato int
 
-        System.out.println("Hola "+ nombre + ". INTRODUCE TU EDAD:");
-        int edad = entrada.nextInt();
-
-        if (edad >= 18){
-            System.out.println("Enhorabuena "+ nombre + ". Puedes votar.");
+        if (edad >= 18){//Si la variable edad es igual o mayor a 18
+            System.out.println("Enhorabuena "+ nombre + ". Puedes votar.");//Texto si la condicion se cumple
         }
-        else {
-            int anyosFaltantes = 18 - edad;
+        else {//Si no se cumple
+            int anyosFaltantes = 18 - edad;//Establezco una variable int anyosFaltantes que su valor sera 18 - la variable edad
             System.out.println("****************************************************************");
-            System.out.println("Lo siento "+ nombre + ". No puedes votar.");
-            System.out.println("Te faltan "+ anyosFaltantes + " años para ser mayor de edad.");
+            System.out.println("Lo siento "+ nombre + ". No puedes votar.");//Aparece por pantalla el resultado de si la condicion no se cumple
+            System.out.println("Te faltan "+ anyosFaltantes + " años para ser mayor de edad.");//Aparece por pantalla los años faltantes
             System.out.println("****************************************************************");
         }
 
