81d792dc98dc10f2d7f148e5567fb6e63bcf0c16
DIEGO\diego <diegogardiol6@gmail.com>
2024-10-17 11:26:05 +0200

    no message

--- EJERCICIOS-Diego_Gardiol/src/main/java/org/example/calculadora.java
+++ EJERCICIOS-Diego_Gardiol/src/main/java/org/example/calculadora.java
@@ -15,28 +15,55 @@ public class calculadora {
         System.out.println("(/) --> DIVISION                      =");
         System.out.println("(R) --> RAIZ CUADRADA                 =");
         System.out.println("=======================================");
+
+
         int op1 = 0;
         int op2 = 0;
-        boolean validInput = false;
-        while (!validInput) {
+        String sig = "";
+
+        boolean salidavalida = false;
+        while (!salidavalida) {
             try {
                 System.out.print("Escribe aqui el primer operador: ");
                 op1 = entrada.nextInt();
-                validInput = true; // Entrada válida, salir del bucle
+                salidavalida = true;
             } catch (InputMismatchException e) {
                 System.out.println("**ERROR: Solo se permiten números enteros. Inténtalo de nuevo.**");
-                entrada.next(); // Limpiar la entrada incorrecta
+                entrada.next();
             }
         }
 
-        System.out.print("Escribe aqui el signo a aplicar: ");
-        String sig = entrada.next();
+
+        salidavalida = false;
+        while (!salidavalida) {
+            System.out.print("Escribe aqui el signo a aplicar: ");
+            sig = entrada.next();
+
+            if (sig.length() != 1) {
+                System.out.println("**ERROR: Debes ingresar un solo carácter. Inténtalo de nuevo.**");
+                continue;
+            }
+            if (Character.isDigit(sig.charAt(0))) {
+                System.out.println("**ERROR: No se permiten números como signo. Inténtalo de nuevo.**");
+            } else {
+                salidavalida = true;
+            }
+        }
 
 
 
         if (!sig.equals("R")) {
-            System.out.print("Escribe aqui el segundo operador: ");
-            op2 = entrada.nextInt();
+            salidavalida = false;
+            while (!salidavalida) {
+                try {
+                    System.out.print("Escribe aqui el segundo operador: ");
+                    op2 = entrada.nextInt();
+                    salidavalida = true;
+                } catch (InputMismatchException e) {
+                    System.out.println("**ERROR: Solo se permiten números enteros. Inténtalo de nuevo.**");
+                    entrada.next();
+                }
+            }
         }
 
         System.out.println("=======================================");
