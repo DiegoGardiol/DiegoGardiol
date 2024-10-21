7533c6d553a690433dd325406cf8ffcaf9137fb6
A716\1dam <diegogardiol6@gmail.com>
2024-10-21 20:08:24 +0200

    no message

---
+++ EJERCICIOS-Diego_Gardiol/src/main/java/org/example/Practica3_Comprobador_de_ISB.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Practica3_Comprobador_de_ISB {
+
+    public void ISB(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduzca ISBN: ");
+        String isbn = entrada.next();
+
+
+    }
+}
