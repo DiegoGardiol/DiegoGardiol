d2d2bcc8a081e28aac90aaf9a29809c167fd4eea
A716\1dam <diegogardiol6@gmail.com>
2024-11-21 16:00:02 +0100

    no message

--- EJERCICIOS-Diego_Gardiol/src/main/java/org/example/Arrays.java
+++ EJERCICIOS-Diego_Gardiol/src/main/java/org/example/Arrays.java
@@ -5,26 +5,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Arrays {
+    public static boolean toString(int[] filas) {
+
+
     public void ejercicio1() {
-        //SUMAR TODOS LOS VALORES DE UN ARRAY ALEATORIO DE 8 VALORES
 
-        int[] array = new int[8]; //Array de 8 elementos de tipo int
-
-        Random random = new Random();
-
-        int suma = 0;
-
-        System.out.println("*****************************************");
-        System.out.println("ESTOS SON LOS ELEMENTOS DE NUESTRO ARRAY:");
-        System.out.println("*****************************************");
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(501); // Generamos un numero aleatorio entre 0 y 500
-            suma += array[i]; //Sumamos el valor de el elemento actual al total
-            System.out.print(array[i] + " ");
-        }
-        System.out.println("\nLa suma de los elementos es: " + suma);
-        System.out.println("*****************************************");
     }
 
     public void ejercicio2() {
--- EJERCICIOS-Diego_Gardiol/src/main/java/org/example/matrices.java
+++ EJERCICIOS-Diego_Gardiol/src/main/java/org/example/matrices.java
@@ -1,17 +1,50 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class matrices {
-    public void prueba(){
+    public void prueba() {
 
-       int matriz[][] = new int[3][4];
+        int matriz[][] = new int[3][4];
 
-        int matriz2[][] = {{12,32,44,1},{33,2,90,56}};
+        int matriz2[][] = {{12, 32, 44, 1}, {33, 2, 90, 56}};
 
 
         System.out.println(matriz2[0][2]);
 
 
+        System.out.println("TAMAÑO FILA: " + matriz2.length);
+        System.out.println("TAMAÑO COLUMNA: " + matriz2[0].length);
 
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                System.out.println(matriz2[i][j] + " ");
+            }
+        }
 
+        for (int[] filas : matriz2) {
+            for (int columnas : filas) {
+                System.out.println(columnas + " ");
+            }
+            System.out.println("\n");
+        }
+
+        System.out.println("-------------------------------------------------------------------\n");
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("Dime el valor de la posicion (" + i + "," + j + "):");
+                matriz[i][j] = entrada.nextInt();
+            }
+
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    System.out.println(matriz[i][j]);
+                }
+                System.out.println("");
+
+            }
+        }
     }
 }
